9b4c84c910b55cd5da48b5a8c2f8f7e5031e53d9
fordring <36187302+trionfordring@users.noreply.github.com>
2019-09-25 17:17:55 +0800

    first push
    ..

---
+++ test/test/test.cpp
@@ -0,0 +1,58 @@
+﻿#include <iostream>
+
+using namespace std;
+
+
+int main1() {
+	
+	int n;
+	int length;
+	int* a;
+	int min = -1;
+	int index;
+	while (cin >> n) {
+		length = n * 2;
+		a = new int[length];
+		for (int i = 0; i < length; i++) {
+			if (i < length / 2) {
+				a[i] = 1;
+			}
+			else {
+				a[i] = 2;
+			}
+		}
+		index = 0;
+		for (int i = 2;min==-1; i++) {
+			while (a[length - 1] != 1) {
+				index =(index + i - 1) % length;;
+				if (a[index] != 2) {
+					break;
+				}
+				else {
+					a[length-1] = 3;
+					length--;
+					if (index == length) {
+						index = 0;
+					}
+				}
+			}
+			if (a[length - 1] == 1) { 
+				min = i;
+			}
+			index = 0;
+			length = n * 2;
+			for (int i = 0; i < length; i++) {
+				if (i < length / 2) {
+					a[i] = 1;
+				}
+				else {
+					a[i] = 2;
+				}
+			}
+		}
+		cout << min << endl;
+		min = -1;
+		delete a;
+	}
+	return 0;
+}
---
+++ test/test/test3.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+
+using namespace std;
+
+int max = 0;
+class point {
+public:
+	int x;
+	int y;
+	point(int x,int y):x(x),y(y){}
+	point(){}
+};
+class block {
+public:
+	int rows;
+	int cols;
+	int length;
+	int* values =nullptr;
+	bool set() {
+		if (!(cin >> rows >> cols)) {
+			return false;
+		}
+		length = rows * cols;
+		if (values != nullptr) {
+			delete values;
+		}
+		values = new int[length];
+		for (int i = 0; i < length; i++)
+		{
+			cin >> values[i];
+		}
+		return true;
+	}
+	void input(point p,int value) {
+		values[p2i(&p)] = value;
+	}
+	int p2i(point* p) {
+		return cols * (p->y) + p->x;
+	}
+	point* i2p(int i) {
+		return new point(i % cols,i/cols);
+	}
+	int slip(point* sp) {
+
+	}
+	point* up(point* sp) {
+		int t = p2i(sp)-cols;
+		if (t >= 0) {
+			return i2p(t);
+		}
+		return nullptr;
+	}
+	point* down(point* sp) {
+		int t = p2i(sp) + cols;
+		if (t < length) {
+			return i2p(t);
+		}
+		return nullptr;
+	}
+	point* left(point* sp) {
+		if (sp->x <= 0)return nullptr;
+		return i2p(p2i(sp) - 1);
+	}
+	point* right(point* sp) {
+		if (sp->x <= 0)return nullptr;
+		return i2p(p2i(sp) - 1);
+	}
+};
+int main() {
+	block b;
+	while (b.set())
+	{
+
+	}
+	return 0;
+}
